bf3228d6a7a09a5b6dd898123b4859022e433ad7
ShuvalovMaxim <shuvalov99.me@gmail.com>
2021-06-12 12:20:43 +0600

    + новый клиент

---
+++ SafeCity/SafeCity.Client/wwwroot/js/appeal.js
@@ -0,0 +1,44 @@
+const urlCategory = 'http://localhost:8090/api/alltypes'
+
+const cateagoryAppeal = document.getElementById('сategoryAppeal');
+
+let fetchAllCategory = fetch(urlCategory)
+
+fetchAllCategory.then(function(response) {
+    response.text().then(function(text) {
+        var sel = document.getElementById("сategoryAppeal");
+        var options = JSON.parse(text)
+
+        for (var i = 0; i <= options.length - 1; i++) {
+            var opt = document.createElement('option');
+            opt.value = options[i].key;
+            opt.text = options[i].name;
+            sel.appendChild(opt);
+        }
+    });
+});
+
+
+function changeOption() {
+    let option = typeSelect.options[typeSelect.selectedIndex];
+    let urlSubType = new URL('http://localhost:8090/api/subtypesbytype?');
+    let params = { nameType: option.text };
+    urlSubType.search = new URLSearchParams(params).toString();
+    let myFethcSubTypes = fetch(urlSubType);
+
+    myFethcSubTypes.then(function(response) {
+        response.text().then(function(text) {
+            var sel = document.getElementById("subtypesappeal");
+            var options = JSON.parse(text)
+
+            for (var i = 0; i <= options.length - 1; i++) {
+                var opt = document.createElement('option');
+                opt.value = options[i].key;
+                opt.text = options[i].name;
+                sel.appendChild(opt);
+            }
+        });
+    });
+}
+
+typeSelect.addEventListener("change", changeOption);
